8413a9410ccbcf4038138a269b57287684e3d95a
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-03-05 09:40:57 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,14 +19,14 @@ def render_response():
     else:
         reply = "Good to know because I do like movies"
     return render_template('response.html', response = reply)
-    @app.route("/response2")
-    def render_response2():
-        answer2 = request.args['answer2']
-        if answer2== 'comedy':
-            reply2 = "I prefer comedy because I like to laugh, but horror movies are still fun to watch"
-        else answer2== 'horror':
-            reply2 = "That's cool! Did you know that my friend starred in La Larona. If you haven't watched it it's pretty scary."
-        return rendertemplate('response2.html', response = reply2)
+@app.route("/response2")
+def render_response2():
+    answer2 = request.args['answer2']
+    if answer2== 'comedy':
+        reply = "I prefer comedy because I like to laugh, but horror movies are still fun to watch"
+    else answer2== 'horror':
+        reply = "That's cool! Did you know that my friend starred in La Larona. If you haven't watched it it's pretty scary."
+    return rendertemplate('response2.html', response = reply)
         
         
                      
