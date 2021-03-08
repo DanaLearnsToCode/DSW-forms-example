54c5d8fb344b5341ae50de68592e13dfcae1b3ea
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-03-08 10:04:35 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,17 +19,17 @@ def render_response():
         reply = "Good to know because I do like movies"
         return render_template('response2.html', response = reply)
             if answer2 == "horror"
-                reply = "That's cool! My friend was one of the main actors in La Larona. If you haven't watched it it's pretty scary.
-                return render_template('response3.1.html', response = reply)
+                reply = "That's cool! My friend was one of the main actors in La Larona. If you haven't watched it it's pretty scary."
+                return render_template('response31.html', response = reply)
             if answer2 == "comedy"
                 reply = "I also prefer comedy because I like to laugh, but horror movies are still fun to watch"
-                return render_template('response3.2.html', response = reply)
+                return render_template('response32.html', response = reply)
             if answer2 == "yes"
                 reply = "I'm also a sports person."
-                return render_template('response3.3.html', response = reply)
+                return render_template('response33.html', response = reply)
             if answer2 == "no"
                 reply = "Wow! most people I know play sports, so this is very cool."
-                return render_template('response3.4.html', response = reply)
+                return render_template('response34.html', response = reply)
             
 
 #@app.route("/home2.html")
